83ae2313b3f709b9ab5a231f3af704689df8ad15
Nihal Kamat <nil.k.2436@gmail.com>
2023-11-28 11:09:52 +0530

    2nd commit

--- services/auth/src/settings.py
+++ services/auth/src/settings.py
@@ -8,3 +8,10 @@ DATABASE = {
     "HOST": os.environ.get("AUTH_DATABASE_HOST"),
     "PORT": os.environ.get("AUTH_DATABASE_PORT"),
 }
+
+SQLALCHEMY_DATABASE_URI = (
+    "postgresql://%(USER)s:%(PASSWORD)s@%(HOST)s:%(PORT)s/%(NAME)s" % DATABASE
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = bool(
+    decode_base64(os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS"))
+)
