4c8cb9bc1f6c449627c6f3f124077cf1ddb62f5a
Nihal Kamat <nil.k.2436@gmail.com>
2023-11-28 11:18:16 +0530

    2nd commit

--- services/auth/src/settings.py
+++ services/auth/src/settings.py
@@ -2,16 +2,14 @@ import os
 from datetime import datetime, timedelta
 
 DATABASE = {
-    "NAME": os.environ.get("AUTH_DATABASE_NAME"),
-    "USER": os.environ.get("AUTH_DATABASE_USER"),
-    "PASSWORD": os.environ.get("AUTH_DATABASE_PASSWORD"),
-    "HOST": os.environ.get("AUTH_DATABASE_HOST"),
-    "PORT": os.environ.get("AUTH_DATABASE_PORT"),
+    "NAME": ,
+    "USER": ,
+    "PASSWORD":, 
+    "HOST": ,
+    "PORT": 
 }
 
-SQLALCHEMY_DATABASE_URI = (
-    "postgresql://%(USER)s:%(PASSWORD)s@%(HOST)s:%(PORT)s/%(NAME)s" % DATABASE
-)
-SQLALCHEMY_TRACK_MODIFICATIONS = bool(
-    decode_base64(os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS"))
-)
+SQLALCHEMY_DATABASE_URI = ("postgresql://%(USER)s:%(PASSWORD)s@%(HOST)s:%(PORT)s/%(NAME)s" % DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = False 
+AUTH_GRPC_CHANNEL = 
+NOTIFICATION_GRPC_CHANNEL = 
