47be4a6ba0692454d5c64f350db64f9ea3099ef8
dhankumari <dhankumari1403@gmail.com>
2023-11-30 11:14:44 +0530

    changes

--- services/auth/src/api/api_urls.py
+++ services/auth/src/api/api_urls.py
@@ -1,6 +1,6 @@
 from app import swagger_api
-from .api_views import Hello, sign_up, Login
+from .api_views import Hello, SignUp, Login
 
 swagger_api.add_resource(Hello, "/hello")
-swagger_api.add_resource(sign_up, "/signup")
+swagger_api.add_resource(SignUp, "/signup")
 swagger_api.add_resource(Login, "/login")
---
+++ services/auth/src/utils.py
@@ -0,0 +1,2 @@
+def signup(self):
+    
