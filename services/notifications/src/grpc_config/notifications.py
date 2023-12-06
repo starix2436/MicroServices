54969def30ff337ed4fe2168045b34a64eb2a830
dhankumari <dhankumari1403@gmail.com>
2023-12-06 09:46:31 +0530

    changes

--- services/notifications/src/grpc_config/notifications.py
+++ services/notifications/src/grpc_config/notifications.py
@@ -1,22 +1,46 @@
 import grpc
-import auth_pb2
-import auth_pb2_grpc
+import grpc_config.auth_pb2
+import grpc_config.auth_pb2_grpc
+import time
+#client 
 
+# def get_client_stream_requests():
+#     while True:
+#         email = input("Please enter a name (or nothing to stop chatting): ")
+
+#         if email == "":
+#             break
+#         detail_request = grpc_config.auth_pb2.UserDetailRequest(id=6)
+#         yield detail_request
+#         time.sleep(1)
 
 def run():
-    # Connect to the gRPC server
-    with grpc.insecure_channel("localhost:50051") as channel:
-        stub = auth_pb2_grpc.NotificationsStub(channel)
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = grpc_config.auth_pb2_grpc.NotificationsStub(channel)
+        
+        detail_request = grpc_config.auth_pb2.UserDetailRequest(id=6)
+        detail_reply =stub.GetUserDetails(detail_request)
+        print(detail_reply)
 
-        # Create a user request with an ID
-        userreq = auth_pb2.UserDetailRequest(id=6)
-
-        # Make a gRPC call to the server and receive the response
-        userres = stub.GetUserDetails(userreq)
-
-        # Process the received user details
-        print(userres.first_name, userres.last_name, userres.email)
-
-
-if __name__ == "__main__":
+if __name__=="__main__":
     run()
+
+
+
+# def run():
+#     # Connect to the gRPC server
+#     with grpc.insecure_channel("localhost:50051") as channel:
+#         stub = auth_pb2_grpc.NotificationsStub(channel)
+
+#         # Create a user request with an ID
+#         userreq = auth_pb2.UserDetailRequest(id=6)
+
+#         # Make a gRPC call to the server and receive the response
+#         userres = stub.GetUserDetails(userreq)
+
+#         # Process the received user details
+#         print(userres.first_name, userres.last_name, userres.email)
+
+
+# if __name__ == "__main__":
+#     run()
